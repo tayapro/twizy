b5395b76ef71eaff8a72aab366eb09573675cb77
Taisiia Simakova <stipaxa@gmail.com>
2024-07-28 17:10:51 +0000

    Implement persistent quiz storage
    - Added a quiz spreadsheet to be used as input for
        the quiz game screen.
    - Introduced new functionality to fetch and cycle
        through 10 random questions for the game.

--- screens/game_screen.py
+++ screens/game_screen.py
@@ -1,5 +1,6 @@
 import sys
 import curses
+import random
 from curses.textpad import rectangle
 from config import consts, layout, palette, game
 from components.greeting import Greeting
@@ -10,7 +11,20 @@ from components.text import Text
 from components.centered_text import CenteredText
 from components.right_text import RightText
 from components.animated_text import AnimatedText
-from lib import local_storage
+from lib import local_storage, spreadsheet_storage
+
+def fetch_quiz_data():
+    return [
+        (question, correct_option, option_0,
+        optiopn_1, option_2, option_3) 
+        for question, correct_option, option_0,
+            optiopn_1, option_2, option_3 
+        in spreadsheet_storage.get_table("quiz")[1:]
+    ]
+
+def set_quiz(quiz_arr):
+    random.shuffle(quiz_arr)
+    return quiz_arr[:10]
 
 def game_screen_handler(stdscr):
     color = curses.color_pair(palette.MAIN_COLOR)
@@ -30,11 +44,20 @@ def game_screen_handler(stdscr):
     ]
 
     question_counter = 1
+    score = 0
 
-    # initialize answers
-    answers_list = ["1. Alien", "2. Monster", "3. Programmer", "4. Who?"]
-    answers = Menu(12, 10, "", *answers_list)
-    question = AnimatedText("Who's Mr. Bean? ", 10, 10, 0.1)
+    quiz_storage_data = fetch_quiz_data()
+    quiz = set_quiz(quiz_storage_data)
+    print(f"QUIZ: {quiz}", file=sys.stderr)
+    print("----------------------------------------", file=sys.stderr)
+
+    # Initialize answers
+    i = 0
+    answers_list = quiz[i][2:6]
+    # answers = Menu(12, 10, "", *answers_list)
+    question = AnimatedText(quiz[i][0], 10, 10, 0.1)
+    # print(f"answers_list: {answers_list}", file=sys.stderr)
+    # print(f"question: {quiz[0][0]}", file=sys.stderr)
 
     while True:
         # Clear screen
@@ -50,6 +73,7 @@ def game_screen_handler(stdscr):
             layout.FRAME_PADDING_TOP, 10
         ).draw(stdscr)
    
+        answers = Menu(12, 10, "", *answers_list)
         answers.draw(stdscr)
         question.draw(stdscr)
 
@@ -68,12 +92,15 @@ def game_screen_handler(stdscr):
             return screen
 
         if question_counter == game.TOTAL_QUESTIONS:
+            local_storage.set_item("score", score)
             return consts.OUTCOME_SCREEN
 
         if code in [10, 13, curses.KEY_ENTER]:
             question_counter += 1
-            answers.set_options("1. Alien", "2. Monster", "3. Programmer", f"4. Who? {question_counter}")
-            question.message = "Are you sure that you are sure? "
+            i += 1
+            answers_list = quiz[i][2:6]
+            answers.set_options(answers_list)
+            question.message = quiz[i][0]
             continue
 
 def on_load_game_screen(w):
